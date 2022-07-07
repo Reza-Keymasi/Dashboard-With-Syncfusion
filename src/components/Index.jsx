dede7a25e5beacb538fb65a1d7c70b0182b934f8
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-07-07 13:28:30 +0430

    Index.jsx is file that i add all of my components there to export them everywhere i want

---
+++ src/components/Index.jsx
@@ -0,0 +1,16 @@
+export { default as  Button } from "./Button.jsx";
+export { default as  ThemeSettings } from "./ThemeSettings.jsx";
+export { default as  Sidebar } from "./Sidebar.jsx";
+
+export { default as  Navbar } from "./Navbar.jsx";
+export { default as  Footer } from "./Footer";
+export { default as  Cart } from "./Cart.jsx";
+export { default as  Chat } from "./Chat.jsx";
+export { default as  UserProfile } from "./UserProfile.jsx";
+export { default as  Notification } from "./Notification.jsx";
+export { default as Header } from "./Header.jsx";
+export { default as  ChartsHeader } from "./ChartsHeader.jsx";
+export { default as  LineChart } from "./charts/LineChart.jsx";
+export { default as  Pie } from "./charts/Pie.jsx";
+export { default as  SparkLine } from "./charts/SparkLine.jsx";
+export { default as  Stacked } from "./charts/Stacked.jsx";
