d29897e658848509640654b1609c27e65ab989fb
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-07-09 01:30:39 +0430

    close div tag take right line

--- src/App.js
+++ src/App.js
@@ -4,9 +4,25 @@ import { FiSettings } from "react-icons/fi"
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import "./App.css"
 
-const App = () => {
 
-  const activeMenu = true;
+// Components
+import { Footer, Navbar, Sidebar, ThemeSettings } from "./components/Index.jsx"
+
+// Pages
+import { 
+  Calender, ColorPicker, Customers, Ecommerce, Editor, Employees, Orders, Kanban,
+  Area, Bar, ColorMapping, Financial, Line, Pie, Pyramid, Stacked 
+} from "./pages/Index.jsx"
+
+
+// context
+import { useStateContext } from "./contexts/ContextProvider"
+
+
+
+const App = () => {
+  const { activeMenu } = useStateContext();
+
 
   return (
     <div>
@@ -27,57 +43,55 @@ const App = () => {
           <div className="w-72 sidebar 
           fixed dark:bg-secondary-dark-bg
           bg-white">
-            sidebar
+            <Sidebar />
           </div>
         ) : (
           <div className="w-0 dark:bg-secondary-dark-bg">
-            sidebar
+            <Sidebar />
           </div>
         )
         }
 
-        <div className={
-          `dark:bg-main-bg bg-main-bg min-h-screen w-full
-          ${activeMenu ? ' md:ml-72 ':'flex-2'}
-          `
-        }>
-          zo zoi
-        </div>
+        <div className={ 
+            `dark:bg-main-bg bg-main-bg min-h-screen w-full
+            ${activeMenu ? ' md:ml-72 ':'flex-2'}
+            `
+          }>
+            
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg
+          navbar w-full">
+            <Navbar />
+          </div>
+        </div>  
 
-        <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg
-        navbar w-full">
-          navbar
-        </div>
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path="/" element={<Ecommerce />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
 
-        <div>
-          <Routes>
-            {/* Dashboard */}
-            <Route path="/" element="Ecommerce" />
-            <Route path="/ecommerce" element="Ecommerce" />
+              {/* Pages */}
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/customers" element={<Customers />} />
 
-            {/* Pages */}
-            <Route path="/orders" element="Orders" />
-            <Route path="/employees" element="Employees" />
-            <Route path="/customers" element="Customers" />
+              {/* Apps */}
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calender" element={<Calender />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
 
-            {/* Apps */}
-            <Route path="/kanban" element="Kanban" />
-            <Route path="/editor" element="Editor" />
-            <Route path="/calender" element="Calender" />
-            <Route path="/color-picker" element="ColorPicker" />
-
-            {/* Charts */}
-            <Route path="/line" element="Line" />
-            <Route path="/area" element="Aria" />
-            <Route path="/bar" element="Bar" />
-            <Route path="/pie" element="Pie" />
-            <Route path="/financial" element="Financial" />
-            <Route path="/color-mapping" element="colorMapping" />
-            <Route path="/pyramid" element="Pyramid" />
-            <Route path="/stacked" element="Stacked" />
-          </Routes>
-        </div>
-
+              {/* Charts */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramid" element={<Pyramid />} />
+              <Route path="/stacked" element={<Stacked />} />
+            </Routes>
+          </div>
       </div>
     </div>
   );
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,16 +1,16 @@
-export { default as Calender } from "./Calender.jsx"
-export { default as Colorpicker } from "./Colorpicker.jsx"
-export { default as Customers } from "./Customers.jsx"
-export { default as Ecommerce } from "./Ecommerce.jsx"
-export { default as Editor } from "./Editor.jsx"
-export { default as Employees } from "./Employees.jsx"
-export { default as Kanban } from "./Kanban.jsx"
-export { default as Orders } from "./Orders.jsx"
-export { default as Area } from "./charts/Area.jsx"
-export { default as Bar } from "./charts/Bar.jsx"
-export { default as ColorMapping} from "./charts/ColorMapping.jsx"
-export { default as Financial } from "./charts/Financial.jsx"
-export { default as Line } from "./charts/Line.jsx"
-export { default as Pie } from "./charts/Pie.jsx"
-export { default as Pyramid } from "./charts/Pyramid.jsx"
-export { default as Stacked } from "./charts/Stacked.jsx"
+export { default as Calender } from "./Calender.jsx";
+export { default as ColorPicker } from "./ColorPicker.jsx";
+export { default as Customers } from "./Customers.jsx";
+export { default as Ecommerce } from "./Ecommerce.jsx";
+export { default as Editor } from "./Editor.jsx";
+export { default as Employees } from "./Employees.jsx";
+export { default as Kanban } from "./Kanban.jsx";
+export { default as Orders } from "./Orders.jsx";
+export { default as Area } from "./charts/Area.jsx";
+export { default as Bar } from "./charts/Bar.jsx";
+export { default as ColorMapping} from "./charts/ColorMapping.jsx";
+export { default as Financial } from "./charts/Financial.jsx";
+export { default as Line } from "./charts/Line.jsx";
+export { default as Pie } from "./charts/Pie.jsx";
+export { default as Pyramid } from "./charts/Pyramid.jsx";
+export { default as Stacked } from "./charts/Stacked.jsx";
