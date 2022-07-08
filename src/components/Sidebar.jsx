71f23b438d72ca54c69813e30c6ddc51a33cbf74
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-07-09 01:33:43 +0430

    onClick(s) defined to control the open and close the Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,12 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { links } from "../data/Data"
 
+// Contexts
+import { useStateContext } from "../contexts/ContextProvider"
+
 const Sidebar = () => {
+
+  const { activeMenu, setActiveMenu } = useStateContext();
 
   const activeLink =
    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
@@ -14,7 +19,7 @@ const Sidebar = () => {
   const normalLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-700 dark:text-gray-200 text-md dark:hove:text-black hover:bg-light-gray m-2";
 
-  const activeMenu = true;
+
 
   return (
     <div className="ml-3 h-screen
@@ -24,14 +29,15 @@ const Sidebar = () => {
         {
           activeMenu && (<>
           <div className="flex justify-between items-center">
-            <Link to="/" onClick={() => {}} className="items-center gap-3 ml-3 mt-4
+            <Link to="/" onClick={() => setActiveMenu(false)} 
+            className="items-center gap-3 ml-3 mt-4
             flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900
             ">
               <SiShopware /> <span>Shoppy</span>
             </Link>
 
             <TooltipComponent content="Menu" position="BottomCenter">
-              <button type="button" onClock={() => {}}
+              <button type="button" onClick={() => setActiveMenu((prevActiveMenu) => (!prevActiveMenu))}
               className="text-xl rounded-full p-3 hover:bg-light-gray
               mt-4 block md:hidden"
               >
@@ -40,7 +46,7 @@ const Sidebar = () => {
             </TooltipComponent>
           </div>
           {/* For Actual Items-Loop For Items With Map */}
-          <div claaName="mt-10">
+          <div className="mt-10">
             {
               links.map((item) => (
                 <div key={item.title}>
