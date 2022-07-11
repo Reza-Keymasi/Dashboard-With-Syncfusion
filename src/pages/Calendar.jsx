c4f80a381542e673ebf4f1b27f5551f20798bfc9
Reza-Keymasi <rezakeymasi1996@gmail.com>
2022-07-12 02:48:02 +0430

    I'm almost done with App part with component like Editor.jsx, ColorPicker.jsx, Calendar.jsx, Kanban.jsx. In ColorPicker.jsx, change function defined that when we choose a color, the pen gets the color

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { ScheduleComponent, Day, Week, WorkWeek, Month,
-Agenda, Inject, Resize, DragAndDrop } from "@syncfusion/ej2-react-schedule";
+ Agenda, Inject, Resize, DragAndDrop } from "@syncfusion/ej2-react-schedule";
 
 
 import { scheduleData } from "../data/Data";
